39a634756846c81de53600f083f2273f51f8fa48
KrishnanM63 <krishnan.devpython46112121@gmail.com>
2025-09-04 05:30:07 +0530

    milk collector

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ const liters=document.querySelector("#liter")
 const selections=document.querySelector("#value")
 const fats=document.querySelector("#fat")
 addfbtn.addEventListener("click",()=>{
+   if (fname.value.trim() !== "" && vlags.value.trim() !== ""){
+
     const result=document.querySelector("#farmersBody").innerHTML+=`<tr><td>${fname.value}</td><td>${vlags.value}</td></tr>`
     const options=document.querySelector("#value").innerHTML+=`<option>${fname.value}</option>`
+    fname.value=""
+    vlags.value=""
+    }else{
+        alert("Fill all fields")
+    }
+  
 })
 collectionbtn.addEventListener("click",()=>{
+    if (selections.value.trim() !== "" && liters.value.trim() !== ""&& fats.value !==""){
+    
     const now=new Date()
     const c_table=document.querySelector("#Collectiontable").innerHTML+=`<tr><td>${selections.value}</td><td>${liters.value}</td><td>${fats.value}</td><td>${now.toString()}</td></tr>`
-
+    }else{
+         alert("Fill all fields")
+    }
 })
